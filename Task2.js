f4b6225e419bf815b5607151dccb55f556edfe47
Dinesh <80633667+Decode-dinesh@users.noreply.github.com>
2021-07-22 09:43:32 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,32 @@
+var obj = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+]
+
+// for loop
+console.log("for loop");
+for (i = 0; i < obj.length; i++) {
+    console.log(obj[i]);
+}
+
+// for in
+console.log("for in");
+for (i in obj) {
+    console.log(obj[i]);
+}
+
+
+//for of
+console.log("for of");
+for (let x of obj) {
+    console.log(x);
+}
+
+// for each
+console.log("for each");
+obj.forEach((details) => {
+    console.log('Person: ' + details.person);
+    console.log('age: ' + details.age);
+    console.log('company: ' + details.company);
+});
