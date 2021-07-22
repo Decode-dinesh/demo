5d6f18dda43b5f776bd561f59ba88a6c6fd1119f
Dinesh <80633667+Decode-dinesh@users.noreply.github.com>
2021-07-22 21:14:13 +0530

    Update HttpTask.js

--- day2/HttpTask.js
+++ day2/HttpTask.js
@@ -12,3 +12,21 @@ function foo(values) {
     }
 
 }
+=============================================================
+    
+    
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+xhr.onload = function() {
+    var data = JSON.parse(this.response);
+    foo(data);
+};
+xhr.send();
+
+function foo(values) {
+    for (i = 0; i < values.length; i++) {
+        let value = values[i];
+        console.log(value.name, value.region, value.subregion, value.population);
+    }
+
+}
