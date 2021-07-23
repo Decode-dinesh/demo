8293c8db6cd77eefbf49e927d702633e4951b1d2
Dinesh <80633667+Decode-dinesh@users.noreply.github.com>
2021-07-23 20:38:11 +0530

    Create task3.js

---
+++ day3/task3.js
@@ -0,0 +1,28 @@
+var resume = [{
+    name: "dinesh",
+    email: "xyz@gmail.com",
+    mobile: 123456789,
+    qualification: {
+        school: {
+            class_X: "avichi Higher secondary school",
+            class_XII: "avichi Higher secondary school"
+        },
+        college: {
+            Bsc: "st joseph college",
+            Msc: "SRM university"
+        }
+    },
+    computerSkills: {
+        programmingLanguage: ['c', 'java', 'python'],
+        Frontend: ['HTML', 'CSS', 'Javascript']
+    },
+    languageKnown: ['tamil', 'english', 'telugu'],
+    personalProfile: {
+        gender: 'male',
+        nationality: 'indian',
+        religion: 'hindu',
+        martialStatus: 'single',
+        dateOfBirth: 28 - 09 - 1997,
+        address: 'chennai.........'
+    }
+}]
